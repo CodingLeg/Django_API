6d1f849b0db51b479d3be01a73acaa02e29e083a
savyarex2205 <savyarex2205@gmail.com>
2021-06-02 19:49:33 +0530

    creating CI

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from main.models import User, Group, Member, Message
 
-# Create your tests here.
+class userTestCase(TestCase):
+    def test_save_user(self):
+        User.objects.create(
+            username='savya', full_name='Savya Sachi', email='savya@gmail.com', password='00000'
+        )
+    
+class groupTestCase(TestCase):
+    def test_save_group(self):
+        Group.objects.create(
+            gpName='Swift', subscribe='Available', role='member'
+        )
+
